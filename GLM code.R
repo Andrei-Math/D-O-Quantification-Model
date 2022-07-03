0a9fa4b498c0a32dc82dc199d64a0bf6687be6fb
Andrei-Math <64988367+Andrei-Math@users.noreply.github.com>
2022-07-03 17:06:39 +0200

    Add files via upload

---
+++ GLM code.R
@@ -0,0 +1,177 @@
+# Navigate to the folder
+setwd("C:/Users/CraciunescuA/OneDrive - Willis Towers Watson/Cyber&D&O Pricing/D&O/Public/Data Prep")
+
+library(dplyr)
+cdata = read.csv("cdata.csv")
+cdata = cdata[,-1] # drop the index column when reading
+
+# store the original data separately
+odata <- cdata
+
+
+# What, if we assume (disregarding Exposure!) that ClaimNb ~ Poi(lambda) ?
+# We can estimate lambda via ML from ...
+
+sum(ClaimNb)/length(ClaimNb)
+
+# ######################
+# # Cleaning unrealistic
+# # number of claims
+# ######################
+# 
+# for (i in (1:n))
+# {
+#   if (odata$ClaimNb[i]>4)
+#   {
+#     cdata$ClaimNb[i]<-4
+#   }
+# }
+# 
+# mean(odata$ClaimNb)
+# mean(cdata$ClaimNb)
+# ClaimNb <- cdata$ClaimNb
+
+################################################################################
+# 1.1.2.b Frequency
+################################################################################
+
+Exposure = max(cdata$YEAR) - min(cdata$YEAR)
+Exposure
+Frequency <- sum(ClaimNb)/sum(Exposure)
+Frequency
+
+################################################################################
+# 2. Learning / Testing data split
+################################################################################
+
+n <- nrow(cdata)
+set.seed(100)
+subsetlearn <- sample(c(1:n), round(0.9*n), replace=FALSE)
+learn <- cdata[subsetlearn,]
+test <- cdata[-subsetlearn,]
+
+######################
+# tests if done correctly
+######################
+n-(nrow(learn)+nrow(test))
+nrow(learn)/n
+nrow(test)/n
+
+################################################################################
+# 3. GLM
+################################################################################
+
+#IndustryGLM <- cbind( unique(learn$SIC_DESC), c(1:length(unique(learn$SIC_DESC))))
+#learn$IndustryGLM <- as.factor(learn$SIC_DESC)
+learn$USAJurisGLM <- as.integer(learn$is_USA_juris)
+learn$REVENUESGLM <- as.numeric(learn$REVENUES)
+learn$REGIONGLM <- as.factor(learn$SUB_REGION)
+learn$EMPLOYEESGLM <- as.numeric(learn$EMPLOYEES)
+
+ClaimNb         <- learn$ClaimNb
+#IndustryGLM     <- learn$IndustryGLM
+USAJurisGLM     <- learn$USAJurisGLM
+REVENUESGLM      <- learn$REVENUESGLM 
+REGIONGLM      <- learn$REGIONGLM
+EMPLOYEESGLM    <- learn$EMPLOYEESGLM
+
+#test$IndustryGLM <- as.factor(test$SIC_DESC)
+test$USAJurisGLM <- as.integer(test$is_USA_juris)
+test$REVENUESGLM <- as.numeric(test$REVENUES)
+test$REGIONGLM <- as.factor(test$SUB_REGION)
+test$EMPLOYEESGLM <- as.numeric(test$EMPLOYEES)
+
+glm_freq = function(formula, glm_name){
+  # GLM model withl all covariates
+  glm<-glm(formula=formula, family=poisson(), data = learn)
+  learn[,glm_name] <- fitted(glm)
+  summary(glm)
+  
+  
+  # ERROR - there are unknown factors in the test set
+  # Faktor 'IndustryGLM' hat neue Stufen ADJUSTMENT AND COLLECTION SERVICES, BEAUTY SHOPS, CANE SUGAR, EXCEPT REFINING, CHEWING AND SMOKING TOBACCO AND SNUFF, COMPUTER AND COMPUTER SOFTWARE STORES, CRUSHED AND BROKEN LIMESTONE, FLOUR AND OTHER GRAIN MILL PRODUCTS, FROZEN SPECIALTIES, NEC, INDUSTRIAL PROCESS FURNACES AND OVENS, JEWELRY, PRECIOUS METAL, MEAT PACKING PLANTS, ORDNANCE AND ACCESSORIES, NEC, PHOTOFINISHING LABORATORIES, PRIMARY METAL PRODUCTS, NEC, TESTING LABORATORIES
+  test[,glm_name] <- predict(glm, newdata=test, type="response")
+  
+  ######################
+  # In-Sample errors
+  ######################
+  
+  in_sample <- 2*( sum( learn[,glm_name] ) - sum( learn$ClaimNb ) + sum( log(( learn$ClaimNb / learn[,glm_name] )^(learn$ClaimNb))))
+  average_in_sample <- in_sample / nrow(learn)
+  cat("in-sample avg error = ", average_in_sample,'\n')
+  
+  ######################
+  # Out-of-Sample errors
+  ######################
+  
+  out_of_sample <- 2*( sum( test[,glm_name] ) - sum( test$ClaimNb ) + sum( log(( test$ClaimNb / test[,glm_name] )^(test$ClaimNb))))
+  average_out_of_sample <- out_of_sample / nrow(test)
+  cat("out-of-sample avg error = ", average_out_of_sample)
+  
+  results <- list(
+    'in-sample avg error'=average_in_sample,
+    'out-of-sample avg error'=average_out_of_sample,
+    'learn' = fitted(glm),
+    'test' = predict(glm, newdata=test, type="response"),
+    'model' = glm
+  )
+  
+  return(results)
+}
+
+results1 = glm_freq(ClaimNb ~  USAJurisGLM + REVENUESGLM + REGIONGLM + EMPLOYEESGLM, 'GLM1')
+results2 = glm_freq(ClaimNb ~  USAJurisGLM, 'GLM2')
+
+################################################################################
+# 4. Tree
+################################################################################
+
+ClaimNb         <- learn$ClaimNb
+IndustryGLM     <- learn$IndustryGLM
+USAJurisGLM     <- learn$USAJurisGLM
+REVENUEGLM      <- learn$REVENUEGLM 
+COUNTRYGLM      <- learn$COUNTRYGLM 
+EMPLOYEESGLM    <- learn$EMPLOYEESGLM
+tree1 <- rpart(cbind(Exposure, ClaimNb) ~ Area + VehPower + VehAge + DrivAge + BonusMalus + VehBrand + VehGas + Density + Region, data = learn , method="poisson", control=rpart.control(xval=1, minbucket=10000, cp=0.0005))
+# rpart.plot( tree1 )
+
+
+######################
+# In-Sample errors
+######################
+
+learn$fit <- predict(tree1, learn, type = 'vector')
+in_sample <- 2*( sum( learn$fit ) - sum( learn$ClaimNb ) + sum( log(( learn$ClaimNb / learn$fit )^(learn$ClaimNb))))
+average_in_sample <- in_sample / nrow(learn)
+
+average_in_sample
+
+######################
+# Out-of-Sample errors
+######################
+
+test$fit <- predict(tree1, test, type = 'vector')
+
+out_of_sample <- 2*( sum( test$fit ) - sum( test$ClaimNb ) + sum( log(( test$ClaimNb / test$fit )^(test$ClaimNb))))
+average_out_of_sample <- out_of_sample / nrow(test)
+average_out_of_sample 
+# data_final$CASESTATUS = sub("Dismissed.+", "Dismissed", data_final$CASESTATUS)
+# 
+# # FREQUENCY MODELLING
+# # frequency features: industry, country, marketcap
+# glm.model <-glm(formula = count ~ COUNTRY_CODE + SIC_DESC + CASESTATUS,
+#              data = data_final, family = poisson)
+# print(summary(glm.model))    
+# 
+# 
+# 
+# #use model to predict value of am
+# newdata = data.frame(SIC_DESC="PHARMACEUTICAL PREPARATIONS", CASESTATUS="Dismissed", COUNTRY_CODE="USA")
+# nb_claims_dismissed = predict(glm.model, newdata, type="response")
+# newdata$CASESTATUS = "Settled"
+# nb_claims_settled = predict(glm.model, newdata, type="response")
+# freq_dismissed = nb_claims_dismissed/(nb_claims_dismissed + nb_claims_settled)
+# freq_dismissed = nb_claims_settled/(nb_claims_dismissed + nb_claims_settled)
+# freq = (nb_claims_dismissed + nb_claims_settled)/32
+# cat("How often a claim occurs for ", newdata$SIC_DESC, "? ", freq*100, "%")
+# cat("How often a claim is dimised for ", newdata$SIC_DESC, "? ", freq_dismissed*100, "%")
